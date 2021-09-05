c5e4b159f4f4ae51bd8d986b6145ed292bda4662
Nikita Kipriyanov <22167001+merlin-vrn@users.noreply.github.com>
2021-09-05 09:40:59 +0300

    Реализована функциональная which_triangle_edge
    * Реализована функция which_triangle_edge
    * Написана вспомогательная к ней is_on_the_edge
    * Надо продумать возможное включение is_on_the_edge в is_to_the_left
    * Проблема со структурой; нужно как-то отслеживать смежные треугольники
    
    Signed-off-by: Nikita Kipriyanov <22167001+merlin-vrn@users.noreply.github.com>

--- tcl-poc/delaunay-0.tcl
+++ tcl-poc/delaunay-0.tcl
@@ -56,6 +56,7 @@ proc lexigraphically_larger { pi pj } {
 
 # выясняет, лежит ли точка pr слева от направленного отрезка pi pj
 proc is_to_the_left { tau_name pr pi pj } {
+    # хочется обрабатывать символические точки "по имени", поэтому пусть на вход подаются их индексы, а не "координаты", и поэтому нам нужна структура tau
     upvar 1 $tau_name tau
     # сначала обработаем "символические" точки (стр. 204)
     if {$pj=="p-1"} {
@@ -80,15 +81,35 @@ proc is_to_the_left { tau_name pr pi pj } {
     lassign $tau($pj) xj yj
     if {($xj-$xi)*($yr-$yi)>($xr-$xi)*($yj-$yi)} {
         return 1
+        # равенство означало бы точку, принадлежащую прямой
     }
     return 0
 }
 
-# выясняет, принадлежит ли точка pr треугольнику tri
-proc is_inside_triangle { tau_name pr tri } {
+# выясняет, принадлежит ли точка ребру (границе некоторого треугольника) TODO: возможно, стоит объединить с is_to_the_left, т.к. логика очень сходная
+proc is_on_the_edge { tau_name pr pi pj } {
+    # хочется обрабатывать символические точки "по имени", поэтому пусть на вход подаются их индексы, а не "координаты", и поэтому нам нужна структура tau
+    upvar 1 $tau_name tau
+    # ни одна точка не может принадлежать "символическому ребру"
+    if {$pj=="p-1"} { return 0 }
+    if {$pj=="p-2"} { return 0 }
+    if {$pi=="p-1"} { return 0 }
+    if {$pi=="p-2"} { return 0 }
+    # если ни одна из точек pi, pj не является символической — распаковываем координаты и используем простую линейную алгебру
+    lassign $tau($pi) xi yi
+    lassign $tau($pr) xr yr
+    lassign $tau($pj) xj yj
+    if {($xj-$xi)*($yr-$yi)==($xr-$xi)*($yj-$yi)} {
+        return 1
+    }
+    return 0
+}
+
+# выясняет, принадлежит ли точка pr треугольнику tri (включая границу)
+proc belongs_to_triangle { tau_name pr tri } {
     upvar 1 $tau_name tau
     lassign [lindex $tau($tri) 0] pi pj pk
-    puts "принадлежность $pr ($tau($pr)) треугольнику $pi ($tau($pi)), $pj ($tau($pj)), $pk ($tau($pk))"
+    puts "принадлежность $pr ($tau($pr)) треугольнику $tri: $pi ($tau($pi)), $pj ($tau($pj)), $pk ($tau($pk))"
     if {[is_to_the_left tau $pr $pi $pj]} {
         puts "... $pr ($tau($pr)) левее отрезка $pi ($tau($pi)), $pj ($tau($pj)) — не принадлежит"
         return 0
@@ -101,6 +122,8 @@ proc is_inside_triangle { tau_name pr tri } {
         puts "... $pr ($tau($pr)) левее отрезка $pk ($tau($pk)), $pi ($tau($pi)) — не принадлежит"
         return 0
     }
+    # поскольку в is_to_the_left все условия строгие, то мы отсеяли все случаи попадания точек *за* границу треугольника,
+    # значит, в оставшемся случае точка находится внутри или на границе треугольника, эти случаи мы различим в функции which_triangle_edge
     puts "... принадлежит"
     return 1
 }
@@ -114,7 +137,7 @@ proc find_triangle { tau_name pr } {
         puts "Треугольник $tri содержит ссылки: [lindex $tau($tri) 1]"
         # проверяем их все
         foreach tri [lindex $tau($tri) 1] {
-            if {[is_inside_triangle tau $pr $tri]} {
+            if {[belongs_to_triangle tau $pr $tri]} {
                 # первый из треугольников, в который попадает наша точка, будет подходящим
                 break
             }
@@ -123,10 +146,25 @@ proc find_triangle { tau_name pr } {
     return $tri
 }
 
-# выясняет, на какую из границ треугольника c индексом tri попала точка (сообщает индексы её концов) или же 0 для внутренней точки
+# выясняет, на какое из рёбер треугольника c индексом tri попала точка (сообщает индексы концов) или же возвращает 0 для внутренней точки
 proc which_triangle_edge { tau_name tri pr } {
     upvar 1 $tau_name tau
-#    ...
+    # функция вызывается в случае, когда уже установлено, что точка принадлежит треугольнику или его границе, и это "листик"
+    puts "какому ребру $tri принадлежит точка $pr?"
+    lassign [lindex $tau($tri) 0] pi pj pk
+    if {[is_on_the_edge tau $pr $pi $pj]} {
+        puts "... $pi $pj"
+        return [list $pi $pj]
+    }
+    if {[is_on_the_edge tau $pr $pj $pk]} {
+        puts "... $pj $pk"
+        return [list $pj $pk]
+    }
+    if {[is_on_the_edge tau $pr $pk $pi]} {
+        puts "... $pk $pi"
+        return [list $pk $pi]
+    }
+    puts "... внутри треугольника"
     return 0
 }
 
@@ -168,10 +206,10 @@ proc find_delaunay { tau_name points } {
     puts "1. Лексикографически верхняя точка: p0 = $highest_point"
     # "2." не предполагается никаких действий; логика этого пункта заложена в проверках принадлежности и валидности
     # "3."
-#    array set tau [list p0 $highest_point p-1 ... p-2 ... t0 {{p0 p-1 p-2} {}}]
+#    array set tau [list p0 $highest_point p-1 {left bottom} p-2 {right top} t0 {{p0 p-1 p-2} {}}]
     set tau(p0) $highest_point
-    set tau(p-1) ...
-    set tau(p-2) ...
+    set tau(p-1) {left bottom}
+    set tau(p-2) {right top}
     # Формат данных о треугольнике: список индексов точек, список индексов "дочерних" треугольников
     # Треугольник создаётся без ссылок (как "листик"); они добавляются при его разбиении или "инвалидации" переворачиванием ребра
     set tau(t0) {{p0 p-1 p-2} {}}
@@ -211,8 +249,11 @@ proc find_delaunay { tau_name points } {
             # "10."
             # "11."
             # "12."
-        } else { # "13."
-            puts "Точка находится на границе $edge, ..."
+        } else {
+            # "13."
+            # второй случай на рис. 9.7 стр. 200
+            puts "Точка находится на границе $edge"
+            # находим смежный по этому ребру треугольник
             # "14."
             # "15."
             # "16."
