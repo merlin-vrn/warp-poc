aacdf28bd4fec7c632dfbfa2c9e165fb42540c9b
Nikita Kipriyanov <22167001+merlin-vrn@users.noreply.github.com>
2021-09-14 21:56:00 +0300

    Реализация очереди на базе корутин tcl. Не должно быть проблем с контекстом, хотя ха-ха и содержимое очереди подсмотреть теперь будет трудновато.

--- tcl-poc/primitives.tcl
+++ tcl-poc/primitives.tcl
@@ -4,53 +4,43 @@ proc ::tcl::mathfunc::sqr {x} { expr {$x*$x} }
 # использование: [nextid prefix] создаёт команду [prefix]
 # эта команда возвращает последовательно значения "prefix0", "prefix1", и. т. д., при каждом очередном вызове
 proc nextid_p { prefix } { yield ; for {set i 0} {true} {incr i} { yield "$prefix$i"} }
-proc nextid { prefix } { coroutine $prefix nextid_p $prefix }
+proc nextid { name prefix } { coroutine $name nextid_p $prefix }
 
-proc priority_queue {name {subcommand ""} args} {
-    # Создаёт команду name, которая функионирует как priority queue с вещественными приоритетами
-    # У этой команды есть подкоманды: add, get, len. add с аргументами priority element; get возвращает очередной элемент с минимальным priority, len сообщает, сколько осталось
-    # структура queue: {prio:float args}
-    switch $subcommand {
-        "add" {
-            upvar 2 $name queue ;# 2 здесь потому, что priority_queue будет вызываться сквозь вновь созданную субкоманду, т.е. переменная доступна через два уровня
-            set nprio [lindex $args 0]
-            set min -1
-            set max [llength $queue]
-            while {$min+1<$max} {
-                set mid [expr {($min+$max)/2}]
-                if {[lindex $queue $mid 0]>$nprio} {
-                    set max $mid
-                } else {
-                    set min $mid
+proc priority_queue_coroutine { } {
+    set queue {} ;# переменная контекста корутины, в которой будет храниться собственно очередь
+    set prev {} ;# сюда будет передаваться результат выполнения команды и выдаваться yield на следующем цикле
+    while {true} {
+        set input [yield $prev] ;# yield вернёт то, что мы передали корутине в аргументе
+        set data [lassign $input cmd] ;# первый элемент input попадёт в cmd, остальные (если есть) в data
+        switch $cmd {
+            "add" {
+                set nprio [lindex $data 0] ;# приоритет элемента - в первом аргументе
+                # бинарный поиск позиции в очереди, в которую помещать этот элемент
+                set min -1
+                set max [llength $queue]
+                while {$min+1<$max} {
+                    set mid [expr {($min+$max)/2}]
+                    if {[lindex $queue $mid 0]>$nprio} {
+                        set max $mid
+                    } else {
+                        set min $mid
+                    }
                 }
+                set queue [linsert $queue $max $data]
+                set prev {} ;# у этой команды пустой ответ
             }
-            set queue [linsert $queue $max $args]
-        }
-        "get" {
-            upvar 2 $name queue
-            set item [lindex $queue 0]
-            set queue [lreplace $queue [set queue 0] 0]
-            return $item
-        }
-        "del" {
-            upvar 2 $name queue
-            set tsite [lindex $args 0]
-            for {set i 0} {$i<[llength $queue]} {incr i} {
-                if {[lindex $queue $i 2]==$tsite} { break }
+            "get" {
+                set queue [lassign $queue prev] ;# первый элемент queue попадёт в prev, остальные в queue
             }
-            set item [lindex $queue $i]
-            set queue [lreplace $queue $i $i]
-            return $item
-        }
-        "len" {
-            upvar 2 $name queue
-            llength $queue
-        }
-        default {
-            upvar 1 $name queue
-            proc $name {subcommand args} "priority_queue $name \$subcommand {*}\$args"
-            set queue {}
-            return $name
+            "len" {
+                set prev [llength $queue]
+            }
         }
     }
+}
+
+proc priority_queue { name } {
+    set coro ${name}_coroutine
+    coroutine $coro priority_queue_coroutine
+    proc $name { args } "$coro \$args"
 }
